aa4a52df83f3855d2fd342371ab033f1a36ac82e
deablofk <deablofk@gmail.com>
2025-04-13 12:36:02 -0400

    feat: CoinsCommand

--- bot/src/main/java/dev/cwby/butecobot/slashcommands/CoinsCommand.java
+++ bot/src/main/java/dev/cwby/butecobot/slashcommands/CoinsCommand.java
@@ -2,45 +2,76 @@ package dev.cwby.butecobot.slashcommands;
 
 import java.awt.Color;
 import java.math.BigDecimal;
-import java.util.Optional;
 
-import dev.cwby.butecobot.ButecoBot;
-import dev.cwby.butecobot.ic.ISlashCommand;
 import dev.cwby.butecobot.ic.annotation.command.SlashCommand;
 import dev.cwby.butecobot.ic.exception.CommandFailException;
-import dev.cwby.butecobot.integration.api.data.UserResponse;
-import media.ButecoEmbeds;
-import media.ButecoMedia;
+import dev.cwby.butecobot.media.ButecoEmbeds;
+import dev.cwby.butecobot.media.ButecoMedia;
+import dev.cwby.butecobot.users.dto.UserCoinHistoryRequest;
+import dev.cwby.butecobot.users.dto.UserRequest;
+import dev.cwby.butecobot.users.dto.UserResponse;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 /**
  * CoinsCommand
  */
 @SlashCommand(name = "coins", description = "Consulta extrato das coins")
-public class CoinsCommand implements ISlashCommand {
+public class CoinsCommand extends SlashBaseCommand {
+	private static final BigDecimal DAILY = new BigDecimal("100");
+
+	@Override
+	public void onCooldown(SlashCommandInteractionEvent event) throws CommandFailException {
+		event.replyEmbeds(ButecoEmbeds.COOLDOWN_EXTRACT).setEphemeral(true).queue();
+	}
 
 	@Override
 	public void onSlashCommandInteraction(SlashCommandInteractionEvent event) throws CommandFailException {
+		Member member = event.getMember();
 		String memberId = event.getMember().getId();
 
-		Optional<UserResponse> userResponse = ButecoBot.REST.getUserByDiscordId(memberId);
-		if (userResponse.isEmpty()) {
-			ButecoBot.REST.registerAndGiveCoins(event.getMember());
+		UserResponse userResponse = client.send(api.getUserByDiscordId(memberId)).orElse(null);
+
+		if (userResponse == null) {
+			var data = client.send(api.createUser(buildUserRequest(member))).get();
+			client.send(api.createCoinHistory(buildCoinHistory(data.id(), "Initial")));
 			event.replyEmbeds(ButecoEmbeds.STARTER_COINS).setEphemeral(true).queue();
 			return;
 		}
 
-		BigDecimal totalCoins = ButecoBot.REST.getTotalCoinsByDiscordId(memberId).get();
-		BigDecimal dailyCoins = new BigDecimal("100");
+		if (!userResponse.receivedInitialCoins()) {
+			client.send(api.createCoinHistory(buildCoinHistory(userResponse.id(), "Initial")));
+			event.replyEmbeds(ButecoEmbeds.STARTER_COINS).setEphemeral(true).queue();
+		}
 
-		EmbedBuilder eb = new EmbedBuilder();
+		BigDecimal totalCoins = userResponse.totalCoins();
+		var builder = new StringBuilder();
+		if (userResponse.canReceiveDailyCoins()) {
+			client.send(api.createCoinHistory(buildCoinHistory(userResponse.id(), "Daily")));
+			builder.append("**+").append(DAILY).append(" diárias**\n");
+			totalCoins = totalCoins.add(DAILY);
+		}
+
+		builder.append("**B$ ").append(totalCoins).append("** coins");
+
+		var eb = new EmbedBuilder();
 		eb.setTitle("Saldo");
-		eb.setDescription("**+" + dailyCoins + " diárias**\n**B$ " + totalCoins + "** coins");
+		eb.setDescription(builder.toString());
 		eb.setThumbnail(ButecoMedia.ONE_COIN);
 		eb.setColor(totalCoins.compareTo(BigDecimal.ZERO) == 0 ? Color.decode("#FF0000") : Color.decode("#00FF00"));
-
 		event.replyEmbeds(eb.build()).queue();
+	}
+
+	public UserRequest buildUserRequest(Member member) {
+		User user = member.getUser();
+		return new UserRequest(member.getId(), member.getEffectiveName(), user.getGlobalName(),
+				user.getEffectiveAvatarUrl(), member.getTimeJoined().toLocalDateTime(), true);
+	}
+
+	public UserCoinHistoryRequest buildCoinHistory(Long id, String type) {
+		return new UserCoinHistoryRequest(id, null, DAILY, type, null);
 	}
 
 }
